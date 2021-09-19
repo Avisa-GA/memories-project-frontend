1d8caad03116780e3f36776ebb1981c572feba35
Avisa-SEI <aap571420@gmail.com>
2021-09-19 16:43:03 -0700

    wip: getPosts

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,8 +1,10 @@
 import * as api from "../api";
 
+// actions creators
+
 export const getPosts = () => async (dispatch) => {
   try {
-    const { data } = api.getPosts();
+    const { data } = api.fetchPosts();
     dispatch({ type: "FETCH_ALL", payload: data });
   } catch (error) {
     console.log(error.message);

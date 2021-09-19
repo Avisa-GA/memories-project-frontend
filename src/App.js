b012c5017decd7df439101ba60614052ed861efa
Avisa-SEI <aap571420@gmail.com>
2021-09-19 14:38:12 -0700

    wip: add styles to App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import memories from "./images/memories.png";
 import Form from "./components/Form/Form";
-import Post from "./components/Posts/Post/Post";
+import Posts from "./components/Posts/Posts";
+import useStyles from "./styles";
 
 export default function App() {
+  const classes = useStyles();
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
           Memories
         </Typography>
-        <img src={memories} alt="memories" height="60" />
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
       </AppBar>
       <Grow in>
         <Container>
@@ -22,7 +29,7 @@ export default function App() {
             spacing={3}
           >
             <Grid xs={12} sm={7}>
-              <Post />
+              <Posts />
             </Grid>
             <Grid xs={12} sm={4}>
               <Form />
